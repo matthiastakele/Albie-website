7784b040802b63eb8563fef571333e732e28f6fd
Matthias Takele <matthias.takele@guild.com>
2026-05-21 10:23:53 -0400

    fix: send contact emails to matthiastakele@gmail.com

--- api/contact.js
+++ api/contact.js
@@ -26,8 +26,8 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        from: 'Albie Website <hello@albie-app.com>',
-        to: 'hello@albie-app.com',
+        from: 'Albie Website <onboarding@resend.dev>',
+        to: 'matthiastakele@gmail.com',
         reply_to: email,
         subject: `[${typeLabels[type] || type}] Message from ${name}`,
         html: `
